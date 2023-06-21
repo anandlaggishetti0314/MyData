7097404f135f765673fb02afaa6ee9b76eb2d6c9
bairidivya1232 <divya.bairi@ojas-it.com>
2023-06-21 17:42:08 +0530

    total loginpage completed

---
+++ Team-2/UI-Flutter/lib/forgot.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ForGotWidget extends StatefulWidget {
+  const ForGotWidget({super.key});
+
+  @override
+  State<ForGotWidget> createState() => _ForGotWidgetState();
+}
+
+class _ForGotWidgetState extends State<ForGotWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        width: 350,
+        height: 300,
+        child: Column(
+          children: [
+            RichText(
+                text: TextSpan(
+                    text: 'Forgot Password',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      color: Colors.black,
+                    ))),
+            SizedBox(
+              height: 50,
+            ),
+            buildText('Email'),
+            TextField(
+              decoration: InputDecoration(
+                  label: Text('Enter Email'),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)))),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Center(child: ElevatedButton(onPressed: forgot, child: Text('submit'),))
+          ],
+        ),
+      ),
+    ));
+  }
+  void forgot()
+  {
+
+  }
+}
+
+Widget buildText(String text) {
+  return Align(
+    alignment: Alignment.topLeft,
+    child: Text(
+      text,
+      style: TextStyle(
+        fontWeight: FontWeight.normal,
+        fontSize: 15,
+      ),
+    ),
+  );
+}
---
+++ Team-2/UI-Flutter/lib/loginpage.dart
@@ -0,0 +1,210 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+class Loginwidget extends StatefulWidget {
+  const Loginwidget({super.key});
+
+  @override
+  State<Loginwidget> createState() => _LoginwidgetState();
+}
+
+class _LoginwidgetState extends State<Loginwidget> {
+  final formfiled = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool passToggle = true;
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Row(
+          children: [
+            Container(
+              
+              child: Image(
+                image: AssetImage("images/Background.png"),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Container(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset('images/Template.png'),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Manage Insurance',
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal, fontSize: 22),
+                          children: [
+                            TextSpan(
+                                text: ' AWESOME ',
+                                style: TextStyle(color: Colors.blue)),
+                            TextSpan(text: 'Insurance')
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    buildText('Email'),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          label: Text('Enter the Email'),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Enter email";
+                          }
+                          bool emailValidator = RegExp(
+                                  r"^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")
+                              .hasMatch(value);
+                          if (!emailValidator) {
+                            return "Enter valid email";
+                          }
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    buildText('Password'),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                        controller: passwordController,
+                        // obscureText: passToggle,
+                        decoration: InputDecoration(
+                            label: const Text('Enter password'),
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  passToggle = !passToggle;
+                                });
+                              },
+                              child: Icon(passToggle
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                            ),
+                            //   prefixIcon: const Icon(Icons.password),
+                            border: const OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10)))),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter email';
+                          } else if (passwordController.text.length < 5) {
+                            return 'Password length should be lessthan 6';
+                          }
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        login();
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Container(
+                          height: 45,
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Center(
+                            child: Text(
+                              'Log in',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ListTile(
+                      title: const Text('forgot Password?'),
+                      onTap: () => Navigator.pushReplacementNamed(
+                          context, 'forgotpassword'),
+                    )
+                  ],
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> login() async {
+    setState(() {
+      isLoading = true;
+    });
+    var url = Uri.parse('http://192.168.5.135:8080/verify');
+    var headers = {
+      'Content-Type': 'application/json',
+    };
+    var data = {
+      'email': emailController.text,
+      'password': passwordController.text,
+    };
+
+    try {
+      final response = await http.post(
+        url,
+        headers: headers,
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 200) {
+        print(response.body);
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Widget buildText(String text) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontWeight: FontWeight.normal,
+          fontSize: 15,
+        ),
+      ),
+    );
+  }
+}
